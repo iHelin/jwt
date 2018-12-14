fd6bcb64fc40096d1f761ad007ba7fd79f6b52d8
iHelin <ihelin@outlook.com>
2018-12-14 16:44:46 +0800

    码云登录

--- src/main/java/me/ianhe/security/config/SercurityConfig.java
+++ src/main/java/me/ianhe/security/config/SercurityConfig.java
@@ -14,9 +14,7 @@ public class SercurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
+                .authorizeRequests().anyRequest().authenticated().and()
                 .oauth2Login();
     }
 
