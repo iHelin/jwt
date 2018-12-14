6372bc8a38f9bd5e3e7ffdaa6e4d2e96bea1afd2
iHelin <ihelin@outlook.com>
2018-12-14 10:04:35 +0800

    github登录

--- src/main/java/me/ianhe/security/config/SercurityConfig.java
+++ src/main/java/me/ianhe/security/config/SercurityConfig.java
@@ -1,16 +1,8 @@
 package me.ianhe.security.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 /**
  * @author iHelin
@@ -19,34 +11,13 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 @EnableWebSecurity(debug = true)
 public class SercurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private AccessDeniedHandler accessDeniedHandler;
-    @Autowired
-    private MyAuthenticationEntryPoint myAuthenticationEntryPoint;
-    @Autowired
-    private AuthenticationFailureHandler authenticationFailureHandler;
-    @Autowired
-    private AuthenticationSuccessHandler authenticationSuccessHandler;
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/").hasAnyRole("USER", "ADMIN")
-                .antMatchers("/test").hasRole("ADMIN")
-                .and().formLogin().loginProcessingUrl("/login").permitAll()
-                .failureHandler(authenticationFailureHandler).successHandler(authenticationSuccessHandler).and()
-//                .logout().permitAll().and()
-                .exceptionHandling().authenticationEntryPoint(myAuthenticationEntryPoint)
-                .accessDeniedHandler(accessDeniedHandler);
-    }
-
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("123456").roles("USER").build());
-        manager.createUser(User.withDefaultPasswordEncoder().username("admin").password("123456").roles("ADMIN").build());
-        return manager;
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
     }
 
 }
