39f1b330495911718ce7138f97b64087bda7e092
iHelin <ihelin@outlook.com>
2018-12-14 14:08:37 +0800

    码云登录

--- src/main/java/me/ianhe/security/SecurityApplication.java
+++ src/main/java/me/ianhe/security/SecurityApplication.java
@@ -2,6 +2,9 @@ package me.ianhe.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +19,11 @@ public class SecurityApplication {
         SpringApplication.run(SecurityApplication.class, args);
     }
 
-    @GetMapping({"/test", "/"})
-    public String get() {
-        return "hello";
+    @GetMapping({"/", "/user"})
+    public Object get() {
+        OAuth2AuthenticationToken authentication = (OAuth2AuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        OAuth2User principal = authentication.getPrincipal();
+        return principal.getAttributes();
     }
+
 }
